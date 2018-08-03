64f840ab0111f3de687acc44094142098a0f12d1
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-08-02 19:29:46 -0500

    gitOlympics

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const olympiansArr=['Stefan','Mark','Nick','Trey','Joe'];
+
+const printListOfOlympians=(olympiansArr)=>
+{
+   olympiansArr.forEach((item)=>{
+    console.log(item);
+  })
+}
+
+printListOfOlympians(olympiansArr);
+
